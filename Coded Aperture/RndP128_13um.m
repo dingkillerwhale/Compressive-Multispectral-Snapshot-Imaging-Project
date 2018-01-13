d2aec080f9e5c55a326624b995c09d44a4b5a134
M_Ding <dingkillerwhale@gmail.com>
2018-01-13 18:12:08 +0000

    Update and rename RndP128_13um.m to Coded Aperture/RndP128_13um.m

--- Coded Aperture/RndP128_13um.m
+++ Coded Aperture/RndP128_13um.m
@@ -2,19 +2,20 @@ close all
 clear all
 clc
 
-% Generate Mask
+% Generate Random Mask Kernel
 
 A = [ 0 1 0 1 ...
       1 1 0 1 ...
       0 1 0 0 ...
       1 0 1 0];
   
-Mat = zeros(128);
+Mat = zeros(128); % Generate 128x128 pattern array
+
 for i = 1 : 4 : 128
     for j = 1 : 4 : 128
-        ind = randperm(16);
-        ker = reshape(ind,4,4);        
-        Mat(i:i+3,j:j+3) = A(ker);
+        ind = randperm(16); % shuffle 16 indices from 4x4 matrix
+        ker = reshape(ind,4,4); % get a new 4x4 matrix        
+        Mat(i:i+3,j:j+3) = A(ker); % add the new matrix into pattern array
     end
 end
 
