4487e6f7da3e0fae3b59762110314d343a77ef0c
M_Ding <dingkillerwhale@gmail.com>
2018-01-15 18:47:14 +0000

    Add files via upload

--- Simulation Data Processing/2018-01-13Data/DataCrop.m
+++ Simulation Data Processing/2018-01-13Data/DataCrop.m
@@ -9,25 +9,33 @@ clc
 % Get current folder path
 current_path = pwd;
 
-%% Select the folder where cali data is
-cali_path = strcat(current_path, '\2018-01-13 calibration data\');
+%% Read simu data and cali data
+
 % Select 256x256 13um x 13um
-cali_256_path = strcat(cali_path, '\Detector Sampling256x256 calibration data\Cali_256.mat');
+cali_path = strcat(current_path, '\2018-01-13 calibration data\');
+cali_256_path = strcat(cali_path, 'Detector Sampling256x256 calibration data\Cali_256.mat');
 
 load(cali_256_path);
+
+
+% Select 256x256 13um x 13um
+simu_path = strcat(current_path, '\2018-01-11 simulation data\');
+simu_256_path = strcat(simu_path, 'simu_256.mat');
+
+load(simu_256_path);
 
 % Read wavelength
 load 'wv_selected.mat'
 
-%% Plot Cali Data
+%% Crop Cali Data and Simu Data
 
-% In OSLO, the design has -0.8169 magnification not close to -1
+% Note: In OSLO, the design has -0.8169 magnification not close to -1
 % Therefore, it's observed that in TracePro the detected cali image has
 % shrinked. 
 
 wv_cali = wv_sel.wavelength(1:2:end)*1000;
 
-cali_central = cali(:, :, 11);
+cali_central = cali(:, :, 16);
 
 cali_central_crop = cali_central(65:128+65-1, 65:128+65-1);
 
@@ -36,9 +44,27 @@ imagesc(cali_central_crop);
 colormap copper;
 colorbar
 
+simu_crop = flip(simu(60:128+65-1+10,65:128+65-1),2)';
 
-img_crop = cali_central_crop(14:102+14-1,14:102+14-1);
 figure;
-imagesc(img_crop);
+imagesc(simu_crop);
 colormap copper;
 colorbar
+
+cali_crop = zeros(size(simu_crop,1),size(simu_crop,2),16);
+
+
+for i = 16 : -1 : 1
+   cali_crop(:,1+16-i:128+16-i,i) = flip(cali(60+16-i:128+60-1+16-i,65:128+65-1,i),2)';     
+end
+
+
+
+
+
+
+
+
+
+
+
